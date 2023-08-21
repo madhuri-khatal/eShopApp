ed08f828bb49ce7e477d64a437aef92570df9109
madhuri-khatal <madhuri.k@kitintellect.com>
2023-08-21 19:06:22 +0530

    infinte scroll

--- app/screens/ProductScreen/ProductList.tsx
+++ app/screens/ProductScreen/ProductList.tsx
@@ -1,60 +1,67 @@
 import React from 'react';
-import {Image, ScrollView, View} from 'react-native';
+import {Image, ScrollView, View, FlatList} from 'react-native';
 import ProductItem from './ProductItem';
 import {useProductContext} from './../../context/ProductContext';
-import {Text} from 'react-native-paper';
+import {ActivityIndicator, Text} from 'react-native-paper';
 
 interface props {
   props: any;
 }
 export default function ProductList(props: any) {
-  const {productByCategoryId} = useProductContext();
+  const {productByCategoryId, isLoading, fetchMoreData, refThreshold} =
+    useProductContext();
   return (
     <>
-      {productByCategoryId.length === 0 ? (
-        <>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: 'center',
-              padding: 15,
-                          }}>
-            <Image
-              style={{
-                width: 400,
-                height: 400,
-                              }}
-              source={{
-                uri: 'https://stores.maxfashion.in/VendorpageTheme/Enterprise/EThemeForMax/images/product-not-found.jpg',
-              }}
-            />
-          </View>
-        </>
-      ) : (
-        <ScrollView style={{flex: 1}}>
-          <View
-            style={{
-              flex: 1,
-              paddingTop: 20,
-              backgroundColor: '#F7F7F7',
-              height: '100%',
-            }}>
-            <View
-              style={{
-                flexDirection: 'row',
-                width: '100%',
-                height: '100%',
-                flexWrap: 'wrap',
-              }}>
-              {productByCategoryId.map((product: any, i: number) => (
-                <View style={{width: '50%', padding: 3}}>
-                  <ProductItem key={i} product={product} />
-                </View>
-              ))}
-            </View>
-          </View>
-        </ScrollView>
-      )}
+     
+      <View
+        style={{
+          flex: 1,
+          paddingTop: 20,
+          backgroundColor: '#F7F7F7',
+          height: '100%',
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            width: '100%',
+            height: '100%',
+            flexWrap: 'wrap',
+          }}>
+          <FlatList
+            nestedScrollEnabled={true}
+            contentContainerStyle={{flex: 1}}
+            style={{flex: 1}}
+            numColumns={2}
+            ItemSeparatorComponent={() => (
+              <View
+                style={{
+                  height: 0.5,
+                  width: '100%',
+                  backgroundColor: '#C8C8C8',
+                }}
+              />
+            )}
+            data={productByCategoryId}
+            onEndReached={() => {
+              console.log('to end reach');
+              fetchMoreData();
+            }}
+            ref={refThreshold}
+            onEndReachedThreshold={0.5}
+            ListFooterComponent={() => (
+              <View style={{flex: 1}}>
+                {isLoading && <ActivityIndicator size="small" />}
+              </View>
+            )}
+            renderItem={({item}) => (
+              <View style={{width: '50%', padding: 3}}>
+                <ProductItem product={item} />
+              </View>
+            )}
+            keyExtractor={(item, i) => i.toString()}
+          />
+        </View>
+      </View>
     </>
   );
 }
