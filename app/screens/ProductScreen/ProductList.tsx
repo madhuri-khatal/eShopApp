bdb73bcf7eb1fab07556cf601a16f680c97520d4
madhuri-khatal <madhuri.k@kitintellect.com>
2023-08-22 17:16:40 +0530

    add product not found imahe and remove <p> tag

--- app/screens/ProductScreen/ProductList.tsx
+++ app/screens/ProductScreen/ProductList.tsx
@@ -12,35 +12,45 @@ export default function ProductList(props: any) {
     useProductContext();
   return (
     <>
-      <FlatList
-        nestedScrollEnabled={true}
-        alwaysBounceVertical
-        numColumns={2}
-        ItemSeparatorComponent={() => (
-          <View
-            style={{
-              height: 10,
-              width: '100%',
-              backgroundColor: '#f7f7f7',
-            }}
-          />
-        )}
-        data={productByCategoryId}
-        onEndReached={async () => {
-          console.log('to end reach');
-          await fetchMoreData();
-        }}
-        ref={refThreshold}
-        onEndReachedThreshold={10}
-        ListFooterComponent={() => (
-          <>
-            {isLoading && <ActivityIndicator size="small" />}
-          </>
-        )}
-        renderItem={({item}) => <ProductItem product={item} />}
-        keyExtractor={(item, i) => i.toString()}
-        scrollEnabled
-      />
+      {productByCategoryId.length === 0 ? (
+        <Image
+          style={{
+            width: 400,
+            height: 400,
+          }}
+          source={{
+            uri: 'https://stores.maxfashion.in/VendorpageTheme/Enterprise/EThemeForMax/images/product-not-found.jpg',
+          }}
+        />
+      ) : (
+        <FlatList
+          nestedScrollEnabled={true}
+          alwaysBounceVertical
+          numColumns={2}
+          ItemSeparatorComponent={() => (
+            <View
+              style={{
+                height: 10,
+                width: '100%',
+                backgroundColor: '#f7f7f7',
+              }}
+            />
+          )}
+          data={productByCategoryId}
+          onEndReached={async () => {
+            console.log('to end reach');
+            await fetchMoreData();
+          }}
+          ref={refThreshold}
+          onEndReachedThreshold={10}
+          ListFooterComponent={() => (
+            <>{isLoading && <ActivityIndicator size="small" />}</>
+          )}
+          renderItem={({item}) => <ProductItem product={item} />}
+          keyExtractor={(item, i) => i.toString()}
+          scrollEnabled
+        />
+      )}
     </>
   );
 }
