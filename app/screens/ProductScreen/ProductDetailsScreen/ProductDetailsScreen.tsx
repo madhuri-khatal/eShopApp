661aaee24e761a17e4a1106a379e50d2a348ea42
PurvaBhide <purva@kitintellect.com>
2023-09-12 18:53:20 +0530

    min max price

--- app/screens/ProductScreen/ProductDetailsScreen/ProductDetailsScreen.tsx
+++ app/screens/ProductScreen/ProductDetailsScreen/ProductDetailsScreen.tsx
@@ -47,6 +47,29 @@ export const ProductDetailsScreen = (props: any) => {
     }
   }, [productById?.description || productById?.short_description]);
 
+  const priceHtml = productById?.price_html;
+  const regex =
+    /<span class="woocommerce-Price-amount amount"><bdi><span class="woocommerce-Price-currencySymbol">&#8377;<\/span>([\d.]+)<\/bdi><\/span>/g;
+
+  const prices = [];
+  let match;
+
+  while ((match = regex.exec(priceHtml)) !== null) {
+    // Extract the numeric price value from each match and add it to the 'prices' array
+    prices.push(parseFloat(match[1]));
+  }
+
+  if (prices.length === 2) {
+    // Now, 'prices' contains both numeric price values
+    const firstPrice = prices[0];
+    const secondPrice = prices[1];
+    console.log('First Price:', firstPrice);
+    console.log('Second Price:', secondPrice);
+  } else {
+    console.log('Prices not found');
+  }
+  const firstPrice = prices[0];
+  const secondPrice = prices[1];
   return (
     <>
       <ScrollView>
@@ -169,6 +192,7 @@ export const ProductDetailsScreen = (props: any) => {
               paddingLeft: 10,
               textTransform: 'capitalize',
               marginBottom: 5,
+              color: '#595555',
             }}>
             {productById?.name}
           </Text>
@@ -176,6 +200,23 @@ export const ProductDetailsScreen = (props: any) => {
           <HTMLView value={shortDescriptionText} stylesheet={styles} />
           <View style={{display: 'flex', flexDirection: 'row'}}>
             <View style={{flex: 1}}>
+              {/* <CurrencyComponent
+                value={secondPrice} {secondPrice}
+                style={{
+                  alignSelf: 'flex-bottom',
+                  fontSize: 24,
+                  paddingTop: 13,
+                  marginLeft: 15,
+                }}
+              /> */}
+              <Text
+                style={{
+                  fontSize: 24,
+                  paddingTop: 13,
+                  marginLeft: 15,
+                }}>
+                ₹ {firstPrice} - ₹ {secondPrice}
+              </Text>
               <CurrencyComponent
                 value={productById?.price}
                 style={{
@@ -220,6 +261,7 @@ export const ProductDetailsScreen = (props: any) => {
               paddingLeft: 15,
               fontWeight: 'bold',
               textAlign: 'justify',
+              color: '#595555',
             }}>
             Product Details
           </Text>
@@ -275,5 +317,6 @@ const styles = {
     fontSize: 16,
     margin: 8,
     textAlign: 'justify',
+    color: '#595555',
   },
 };
