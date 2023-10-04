efa0c9f82feb3c0f13eafa4fecbdf1b416aa8112
PurvaBhide <purva@kitintellect.com>
2023-10-04 16:40:35 +0530

    create: coupon ui

--- app/components/Product/CouponList.tsx
+++ app/components/Product/CouponList.tsx
@@ -1,118 +1,48 @@
-// import {useProductContext} from '../../context/ProductContext';
-// import React, {useEffect} from 'react';
-// import {View, FlatList, Text, StyleSheet} from 'react-native';
-
-// const CouponList = () => {
-//   const {couponData} = useProductContext();
-//   const couponCodes = couponData?.map((coupon) => {coupon.code});
-// //   [0]?.amount
-// //   .map(coupon => coupon.code);
-//   console.log(couponCodes);
-//   //   useEffect(() => {
-//   //     // Log only the coupon codes
-//   //     const couponCodes = couponData.map(coupon => coupon.code);
-//   //     console.log(couponCodes);
-//   //   }, [coupons]);
-//   return (
-//     <View style={styles.container}>
-//       <FlatList
-//         data={couponData}
-//         keyExtractor={}
-//         numColumns={2}
-//         renderItem={({item}) => (
-//           <View style={styles.couponItem}>
-//             <Text style={styles.couponTitle}>{couponCodes}</Text>
-//             {/* <Text style={styles.couponDescription}>{item.description}</Text> */}
-//           </View>
-//         )}
-//       />
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     padding: 16,
-//   },
-//   couponItem: {
-//     backgroundColor: 'white',
-//     marginBottom: 16,
-//     padding: 16,
-//     borderRadius: 8,
-//     elevation: 2,
-//     flex: 1, // Make each coupon item take half the space horizontally
-//     margin: 8, // Add margin between items
-//   },
-//   couponTitle: {
-//     fontSize: 18,
-//     fontWeight: 'bold',
-//   },
-//   couponDescription: {
-//     fontSize: 16,
-//     marginTop: 8,
-//   },
-// });
-
-// export default CouponList;
-
-// import {View, Text} from 'react-native';
-// import React from 'react';
-// import {useProductContext} from '../../context/ProductContext';
-
-// export default function CouponList() {
-//   const {couponData} = useProductContext();
-//   // console.log(couponData[0].code);
-
-//   return (
-//     <>
-//       {couponData?.map(item => {
-//         // console.log(item?.code);
-
-//         <View style={{flex: 1, flexDirection: 'row'}}>
-//           <View style={{width: '50%'}}>
-//             <Text style={{color: 'red', backgroundColor: 'black'}}>
-//               item?.code
-//             </Text>
-//           </View>
-//         </View>;
-//       })}
-//     </>
-//   );
-// }
-
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 import {useProductContext} from '../../context/ProductContext';
 import {FlatList} from 'react-native-gesture-handler';
+import {Image} from 'react-native';
 
 export default function CouponList() {
   const {couponData} = useProductContext();
-  console.log(couponData);
   const getRandomColor = () => {
     const colors = [
-      '#e3174b',
-      '#e83838',
-      '#a19999',
-      '#1f251e',
-      '#6f30cf',
-      '#467712',
-      '#000080',
+      '#007bff',
+      '#17a2b8',
+      '#28a745',
+      '#e95d2a',
+      '#dc3545',
+      '#6c757d',
     ];
-
+    for (let i = colors.length - 5; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [colors[i], colors[j]] = [colors[j], colors[i]];
+    }
     const randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
   };
+
   return (
     <View
       style={{
         borderRadius: 10,
-        marginVertical: 16,
-        marginTop: 30,
-        justifyContent: 'center',
+        backgroundColor: '#c8e6ca',
         alignItems: 'center',
         padding: 5,
+        marginVertical: 4,
+        marginHorizontal: 4,
       }}>
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: 'bold',
+          lineHeight: 28,
+          paddingTop: 30,
+          textAlign: 'center',
+        }}>
+        SPECIAL OFFERS
+      </Text>
       <FlatList
         data={couponData}
         keyExtractor={(item: any) => item.id.toString()}
@@ -120,24 +50,67 @@ export default function CouponList() {
         renderItem={({item}: any) => (
           <View
             style={{
-              // borderRadius: 20,
-              marginBottom: 5,
-              width: '50%',
-              padding: 8,
+              width: '49%',
+              marginVertical: 4,
               marginRight: 4,
-              backgroundColor: getRandomColor(),
+              elevation: 5,
+              borderRadius: 10,
+              backgroundColor: '#ffffff',
             }}>
+            <View style={{flexDirection: 'row'}}>
+              <View
+                style={{
+                  flexDirection: 'column',
+                  width: '60%',
+                  // backgroundColor: '#000',
+                }}>
+                <Text
+                  style={{
+                    fontWeight: '600',
+                    fontSize: 18,
+                    padding: 10,
+                    paddingBottom: 0,
+                    marginTop: 15,
+                    textAlign: 'center',
+                  }}>
+                  {item.amount}₹ Off
+                </Text>
+                <Text
+                  style={{
+                    fontWeight: '600',
+                    fontSize: 14,
+                    paddingBottom: 15,
+                    color: '#c2c2c2',
+                    textAlign: 'center',
+                  }}>
+                  {item.code}
+                </Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: 'column',
+                  justifyContent: 'flex-end',
+                }}>
+                <Image
+                  source={require('../../../assets/image/offer.png')}
+                  style={{
+                    width: 65,
+                    height: 65,
+                    justifyContent: 'center',                                                                                                                                               
+                  }}
+                />
+              </View>
+            </View>
+
             <Text
               style={{
-                fontWeight: 'bold',
-                fontSize: 20,
-                color: '#ffffff',
-                textAlign: 'center',
-                margin: 5,
+                fontSize: 14,
+                padding: 10,
+                marginTop: 5,
+                borderTopColor: '#e0e0e0',
+                borderTopWidth: 2,
+                borderStyle: 'dotted',
               }}>
-              {item.code}
-            </Text>
-            <Text style={{fontSize: 16, color: 'black'}}>
               {item.description}
             </Text>
           </View>
@@ -146,3 +119,36 @@ export default function CouponList() {
     </View>
   );
 }
+const styles = StyleSheet.create({
+  dWrapper: {
+    backgroundColor: '#fff',
+  },
+
+  zigZagTop: {
+    marginVertical: 32,
+    backgroundColor: '#1ba1e2',
+    paddingVertical: 32,
+  },
+
+  zigZagBottom: {
+    marginVertical: 32,
+    backgroundColor: '#1ba1e2',
+    paddingVertical: 32,
+  },
+
+  zigZagTopGradient: {
+    height: 32,
+    width: '100%',
+    position: 'relative',
+    bottom: 64,
+    left: 0,
+  },
+
+  zigZagBottomGradient: {
+    height: 32,
+    width: '100%',
+    position: 'relative',
+    top: 64,
+    left: 0,
+  },
+});
