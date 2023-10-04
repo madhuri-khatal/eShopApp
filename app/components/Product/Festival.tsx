492897aa6ab7aaf2ec4b197bd226db99b62f5e2f
madhuri-khatal <madhuri.k@kitintellect.com>
2023-10-04 14:57:55 +0530

    fix : why shgeShop ui

---
+++ app/components/Product/Festival.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Festival() {
+  return (
+    <View>
+      <Text>Festival</Text>
+    </View>
+  )
+}
--- app/components/Product/HomeWhyUs.tsx
+++ app/components/Product/HomeWhyUs.tsx
@@ -4,97 +4,169 @@ import {Card} from 'react-native-paper';
 
 export default function HomeWhyUs() {
   return (
-    <View style={{backgroundColor: '#ebf8fa'}}>
-      <Card
+    <View style={{backgroundColor: '#fae8e7',paddingBottom:20}}>
+     
+
+      <View
         style={{
-          justifyContent: 'center',
+          // justifyContent: 'center',
           backgroundColor: 'white',
-          margin: 15,
+          margin: 10,
+          flex: 1,
+          flexDirection: 'row',
+          width: '95%',
+          borderRadius:15,
+          paddingBottom:10
         }}>
-        <Card.Cover
-          source={{
-            uri: 'https://shgeshop.com/wp-content/uploads/2023/09/Untitled_design__34_-removebg-preview.png',
-          }}
-          style={{
-            height: 100,
-            width: 100,
-            alignSelf: 'center',
-            backgroundColor: 'white',
-            borderRadius: 50,
-            marginHorizontal: 25,
-          }}
-        />
-        <Card.Content>
-          <Text style={{fontSize: 20, fontWeight: 'bold', lineHeight: 28}}>
+        <View style={{width: '20%'}}>
+          <Card.Cover
+            source={{
+              uri: 'https://shgeshop.com/wp-content/uploads/2023/09/Untitled_design__34_-removebg-preview.png',
+            }}
+            style={{
+              height: 90,
+              width: 90,
+              // alignSelf: 'center',
+              backgroundColor: 'white',
+              borderRadius: 50,
+              // marginHorizontal: 25,
+            }}
+          />
+        </View>
+        <View style={{width: '80%'}}>
+          <Card.Content>
+            <Text style={{marginTop:9,fontSize: 20, fontWeight: 'bold', lineHeight: 28}}>
             Home Made
-          </Text>
-          <Text style={{fontSize: 16, lineHeight: 20}}>
+            </Text>
+            <Text style={{fontSize: 16, lineHeight: 20}}>
             Crafted with Care, Made from Home by Self Help Group Members.
             Experience Authenticity in Every Product.
-          </Text>
-        </Card.Content>
-      </Card>
+            </Text>
+          </Card.Content>
+        </View>
+      </View>
 
-      <Card
+      
+      <View
         style={{
-          justifyContent: 'center',
+          // justifyContent: 'center',
           backgroundColor: 'white',
-          margin: 15,
+          margin: 10,
+          flex: 1,
+          flexDirection: 'row',
+          width: '95%',
+          borderRadius:15,
+          paddingBottom:10
         }}>
-        <Card.Cover
-          source={{
-            uri: 'https://shgeshop.com/wp-content/uploads/2023/09/Untitled_design__35_-removebg-preview.png',
-          }}
-          style={{
-            height: 100,
-            width: 100,
-            alignSelf: 'center',
-            backgroundColor: 'white',
-            borderRadius: 50,
-            marginHorizontal: 25,
-          }}
-        />
-        <Card.Content>
-          <Text style={{fontSize: 20, fontWeight: 'bold', lineHeight: 28}}>
-            Quality Food
-          </Text>
-          <Text style={{fontSize: 16, lineHeight: 20}}>
-            Only the finest ingredients ensure our food’s exceptional quality
-          </Text>
-        </Card.Content>
-      </Card>
+        <View style={{width: '20%'}}>
+          <Card.Cover
+            source={{
+              uri: 'https://shgeshop.com/wp-content/uploads/2023/09/Untitled_design__35_-removebg-preview.png',
+            }}
+            style={{
+              height: 90,
+              width: 90,
+              // alignSelf: 'left',
+              backgroundColor: 'white',
+              borderRadius: 50,
+              // marginHorizontal: 25,
+            }}
+          />
+        </View>
+        <View style={{width: '80%'}}>
+          <Card.Content>
+            <Text style={{marginTop:9,fontSize: 20, fontWeight: 'bold', lineHeight: 28}}>
+              Quality Food
+            </Text>
+            <Text style={{fontSize: 16, lineHeight: 20}}>
+              Only the finest ingredients ensure our food’s exceptional quality
+            </Text>
+          </Card.Content>
+        </View>
+      </View>
 
-      <Card
+
+      
+      <View
         style={{
-          justifyContent: 'center',
+          // justifyContent: 'center',
           backgroundColor: 'white',
-          margin: 15,
+          margin: 10,
+          flex: 1,
+          flexDirection: 'row',
+          width: '95%',
+          borderRadius:15,
+          paddingBottom:10
         }}>
-        <Card.Cover
-          source={{
-            uri: 'https://shgeshop.com/wp-content/uploads/2023/09/Untitled_design__36_-removebg-preview.png',
-          }}
-          style={{
-            height: 100,
-            width: 100,
-            alignSelf: 'center',
-            backgroundColor: 'white',
-            borderRadius: 50,
-            marginHorizontal: 25,
-          }}
-        />
-        <Card.Content>
-          <Text style={{fontSize: 20, fontWeight: 'bold', lineHeight: 28}}>
+        <View style={{width: '20%'}}>
+          <Card.Cover
+            source={{
+              uri: 'https://shgeshop.com/wp-content/uploads/2023/09/Untitled_design__36_-removebg-preview.png',
+            }}
+            style={{
+              height: 90,
+              width: 90,
+              // alignSelf: 'center',
+              backgroundColor: 'white',
+              borderRadius: 50,
+              // marginHorizontal: 25,
+            }}
+          />
+        </View>
+        <View style={{width: '80%'}}>
+          <Card.Content>
+            <Text style={{marginTop:9,fontSize: 20, fontWeight: 'bold', lineHeight: 28}}>
             Women Empowerment
-          </Text>
-          <Text style={{fontSize: 16, lineHeight: 20}}>
+            </Text>
+            <Text style={{fontSize: 16, lineHeight: 20}}>
             Empowering women strengthens communities, economies, and the world
             at large.
-          </Text>
-        </Card.Content>
-      </Card>
+            </Text>
+          </Card.Content>
+        </View>
+      </View>
 
-      <Card
+      
+      <View
+        style={{
+          // justifyContent: 'center',
+          backgroundColor: 'white',
+          margin: 10,
+          flex: 1,
+          flexDirection: 'row',
+          width: '95%',
+          borderRadius:15,
+          paddingBottom:10
+        }}>
+        <View style={{width: '20%'}}>
+          <Card.Cover
+            source={{
+              uri: 'https://shgeshop.com/wp-content/uploads/2023/09/Untitled_design__38_-removebg-preview.png',
+            }}
+            style={{
+              height: 90,
+              width: 90,
+              // alignSelf: 'center',
+              backgroundColor: 'white',
+              borderRadius: 50,
+              // marginHorizontal: 25,
+            }}
+          />
+        </View>
+        <View style={{width: '80%'}}>
+          <Card.Content>
+            <Text style={{marginTop:9,fontSize: 20, fontWeight: 'bold', lineHeight: 28}}>
+            Rural Development
+            </Text>
+            <Text style={{fontSize: 16, lineHeight: 20}}>
+            Rural development lays the foundation for sustainable, inclusive
+            growth
+            </Text>
+          </Card.Content>
+        </View>
+      </View> 
+
+     {/* <Card
         style={{
           justifyContent: 'center',
           backgroundColor: 'white',
@@ -118,11 +190,10 @@ export default function HomeWhyUs() {
             Rural Development
           </Text>
           <Text style={{fontSize: 16, lineHeight: 20}}>
-            Rural development lays the foundation for sustainable, inclusive
-            growth
+          
           </Text>
         </Card.Content>
-      </Card>
+      </Card> */}
     </View>
   );
 }
