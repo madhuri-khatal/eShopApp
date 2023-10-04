a56f7e7b27a3a047ca5afc3ed0cf2b804ff8d5c7
madhuri-khatal <madhuri.k@kitintellect.com>
2023-10-04 17:23:28 +0530

    fix :footer

--- app/components/Product/Footer.tsx
+++ app/components/Product/Footer.tsx
@@ -1,22 +1,30 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
+import {View, Text, Image} from 'react-native';
+import React from 'react';
 
 export default function Footer() {
   return (
-<>
-<View>
-    {/* <View style={{flex:1,flexDirection:'row'}}>
-        <View style={{width:'20%'}}>
-            <Image source={{uri:'https://shgeshop.com/wp-content/uploads/2020/12/icons8-headset-45.png'}} 
-            style={{width:40,height:40}}/>
-             </View> */}
-        <View style={{width:'80%'}}>  
-      <Text>	
-Need help? Call us: (+91) 8459172859</Text>
-    </View>
-    {/* </View> */}
-</View>
-</>
-    
-  )
+    <>
+      <View style={{paddingBottom: 10}}>
+        <View style={{flex: 1, flexDirection: 'row', margin: 30}}>
+          <View style={{width: '15%'}}>
+            <Image
+              source={{
+                uri: 'https://shgeshop.com/wp-content/uploads/2020/12/icons8-headset-45.png',
+              }}
+              style={{width: 45, height: 45}}
+            />
+          </View>
+          <View style={{width: '85%'}}>
+            <Text style={{fontWeight: 'bold'}}>
+              Need help? Call us: (+91) 8459172859
+            </Text>
+            <Text>
+              Monday - Friday: 8:00 - 21:00 Saturday - Sunday 9:00 - 18:00
+            </Text>
+          </View>
+        </View>
+
+              </View>
+    </>
+  );
 }
