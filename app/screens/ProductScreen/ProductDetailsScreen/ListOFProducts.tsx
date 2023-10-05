035f425c74be705e290cad5dd989ec4bf0773996
madhuri-khatal <madhuri.k@kitintellect.com>
2023-10-05 18:51:08 +0530

    fix : display only selected products

--- app/screens/ProductScreen/ProductDetailsScreen/ListOFProducts.tsx
+++ app/screens/ProductScreen/ProductDetailsScreen/ListOFProducts.tsx
@@ -1,157 +1,206 @@
+// // import React from 'react';
+// // import {Image, ScrollView, View, FlatList} from 'react-native';
+// // import {ActivityIndicator, Text} from 'react-native-paper';
+// // import ProductItem from '../ProductItem';
+// // import { useProductContext } from '../../../context/ProductContext';
+
+// // export default function listOFProducts() {
+// //   const {productByCategoryId, isLoading, fetchMoreData, refThreshold} =
+// //     useProductContext();
+// //   const noMoreProducts = productByCategoryId.length === 0;
+
+// // //   // Determine the data source for the FlatList
+// // //   const dataToShow =
+// // //     filteredProducts.length > 0 ? filteredProducts : productByCategoryId;
+
+// //   return (
+//     // <>
+//     //   {productByCategoryId.length === 0 ? (
+//     //     <>
+//     //       <View
+//     //         style={{
+//     //           height: '100%',
+//     //           justifyContent: 'center',
+//     //           alignItems: 'center',
+//     //         }}>
+//     //         <ActivityIndicator
+//     //           size="large"
+//     //           color="#e95d2a"
+//     //           style={{backgroundColor: 'white'}}
+//     //         />
+//     //       </View>
+//     //     </>
+//     //   ) : (
+//     //     <FlatList
+//     //       nestedScrollEnabled={true}
+//     //       alwaysBounceVertical
+//     //       numColumns={2}
+//     //       ItemSeparatorComponent={() => (
+//     //         <View
+//     //           style={{
+//     //             width: '100%',
+//     //             backgroundColor: '#f7f7f7',
+//     //           }}
+//     //         />
+//     //       )}
+//     //       data={''} // Use dataToShow as the data source
+//     //       onEndReached={async () => {
+//     //         await fetchMoreData();
+//     //       }}
+//     //       ref={refThreshold}
+//     //       onEndReachedThreshold={10}
+//     //       ListFooterComponent={() =>
+//     //         isLoading ? (
+//     //           <ActivityIndicator
+//     //             size="large"
+//     //             color="#e95d2a"
+//     //             style={{paddingBottom: 70}}
+//     //           />
+//     //         ) : (
+//     //           noMoreProducts && (
+//     //             <View
+//     //               style={{
+//     //                 height: '50%',
+//     //                 justifyContent: 'center',
+//     //                 alignItems: 'center',
+//     //               }}>
+//     //               <Text style={{color: '#b1b1b1'}}>Product end</Text>
+//     //             </View>
+//     //           )
+//     //         )
+//     //       }
+//     //       renderItem={({item}) => <ProductItem product={item} />}
+//     //       keyExtractor={(item, i) => i.toString()}
+//     //       scrollEnabled
+//     //     />
+//     //   )}
+//     // </>
+// //   );
+// // }
+
 // import React from 'react';
 // import {Image, ScrollView, View, FlatList} from 'react-native';
 // import {ActivityIndicator, Text} from 'react-native-paper';
 // import ProductItem from '../ProductItem';
 // import { useProductContext } from '../../../context/ProductContext';
 
-// export default function listOFProducts() {
-//   const {productByCategoryId, isLoading, fetchMoreData, refThreshold} =
+// export default function ListOFProducts() {
+//     const {productByCategoryId, isLoading, fetchMoreData, refThreshold} =
 //     useProductContext();
+//     console.log("HOME PAGE==========================",productByCategoryId.map((item)=>item.name));
+//     const data= productByCategoryId.map((item)=>item.name)
+//     const Product=["Murmure", "Modak Flour" ]
+
 //   const noMoreProducts = productByCategoryId.length === 0;
 
-// //   // Determine the data source for the FlatList
-// //   const dataToShow =
-// //     filteredProducts.length > 0 ? filteredProducts : productByCategoryId;
-
 //   return (
-    // <>
-    //   {productByCategoryId.length === 0 ? (
-    //     <>
-    //       <View
-    //         style={{
-    //           height: '100%',
-    //           justifyContent: 'center',
-    //           alignItems: 'center',
-    //         }}>
-    //         <ActivityIndicator
-    //           size="large"
-    //           color="#e95d2a"
-    //           style={{backgroundColor: 'white'}}
-    //         />
-    //       </View>
-    //     </>
-    //   ) : (
-    //     <FlatList
-    //       nestedScrollEnabled={true}
-    //       alwaysBounceVertical
-    //       numColumns={2}
-    //       ItemSeparatorComponent={() => (
-    //         <View
-    //           style={{
-    //             width: '100%',
-    //             backgroundColor: '#f7f7f7',
-    //           }}
-    //         />
-    //       )}
-    //       data={''} // Use dataToShow as the data source
-    //       onEndReached={async () => {
-    //         await fetchMoreData();
-    //       }}
-    //       ref={refThreshold}
-    //       onEndReachedThreshold={10}
-    //       ListFooterComponent={() =>
-    //         isLoading ? (
-    //           <ActivityIndicator
-    //             size="large"
-    //             color="#e95d2a"
-    //             style={{paddingBottom: 70}}
-    //           />
-    //         ) : (
-    //           noMoreProducts && (
-    //             <View
-    //               style={{
-    //                 height: '50%',
-    //                 justifyContent: 'center',
-    //                 alignItems: 'center',
-    //               }}>
-    //               <Text style={{color: '#b1b1b1'}}>Product end</Text>
-    //             </View>
-    //           )
-    //         )
-    //       }
-    //       renderItem={({item}) => <ProductItem product={item} />}
-    //       keyExtractor={(item, i) => i.toString()}
-    //       scrollEnabled
-    //     />
-    //   )}
-    // </>
-//   );
+//     <>
+//     {productByCategoryId.length === 0 ? (
+//       <>
+//         <View
+//           style={{
+//             height: '100%',
+//             justifyContent: 'center',
+//             alignItems: 'center',
+//           }}>
+//           <ActivityIndicator
+//             size="large"
+//             color="#e95d2a"
+//             style={{backgroundColor: 'white'}}
+//           />
+//         </View>
+//       </>
+//     ) : (
+//       <FlatList
+//         nestedScrollEnabled={true}
+//         alwaysBounceVertical
+//         numColumns={2}
+//         ItemSeparatorComponent={() => (
+//           <View
+//             style={{
+//               width: '100%',
+//               backgroundColor: '#f7f7f7',
+//             }}
+//           />
+//         )}
+//         data={productByCategoryId} // Use dataToShow as the data source
+//         // onEndReached={async () => {
+//         //   await fetchMoreData();
+//         // }}
+//         ref={refThreshold}
+//         onEndReachedThreshold={10}
+//              renderItem={({item}) => <ProductItem product={item} />}
+//         keyExtractor={(item, i) => i.toString()}
+//         scrollEnabled
+//       />
+//     )}
+//   </>
+//   )
 // }
 
+
+
 import React from 'react';
-import {Image, ScrollView, View, FlatList} from 'react-native';
-import {ActivityIndicator, Text} from 'react-native-paper';
+import { Image, ScrollView, View, FlatList } from 'react-native';
+import { ActivityIndicator, Text } from 'react-native-paper';
 import ProductItem from '../ProductItem';
 import { useProductContext } from '../../../context/ProductContext';
 
 export default function ListOFProducts() {
-    const {productByCategoryId, isLoading, fetchMoreData, refThreshold} =
-    useProductContext();
-    // console.log("HOME PAGE==========================",productByCategoryId);
-    
-  const noMoreProducts = productByCategoryId.length === 0;
+  const { productByCategoryId, isLoading, fetchMoreData, refThreshold } = useProductContext();
+  const Product = [ "Masala Banana Chips", "Pudina Banana Chips", 
+//   "Tomato Banana Chips","Nagli/Ragi Biscuit","Rajgira Ladoo", "Shevga/Moringa Powder", "Organic jaggery powder",
+//   "Kalna flour (कळणं पीठ)",
+  "Beet Root Powder","Batti Flour", "Moong Dal Chilla/Edani (मुगाची ऐडणी)","Millet flour (बाजरी पीठ)", "Murmure",];
+
+
+  // Filter the data to include only the products in the Product array
+  console.log("HOME PAGE==========================",productByCategoryId.map((item)=>item.name));
+  const filteredData = productByCategoryId.filter(item => Product.includes(item.name));
+
+  const noMoreProducts = filteredData.length === 0;
 
   return (
     <>
-    {productByCategoryId.length === 0 ? (
-      <>
-        <View
-          style={{
-            height: '100%',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <ActivityIndicator
-            size="large"
-            color="#e95d2a"
-            style={{backgroundColor: 'white'}}
-          />
-        </View>
-      </>
-    ) : (
-      <FlatList
-        nestedScrollEnabled={true}
-        alwaysBounceVertical
-        numColumns={2}
-        ItemSeparatorComponent={() => (
+      {filteredData.length === 0 ? (
+        <>
           <View
             style={{
-              width: '100%',
-              backgroundColor: '#f7f7f7',
-            }}
-          />
-        )}
-        data={productByCategoryId} // Use dataToShow as the data source
-        onEndReached={async () => {
-          await fetchMoreData();
-        }}
-        ref={refThreshold}
-        onEndReachedThreshold={10}
-        // ListFooterComponent={() =>
-        //   isLoading ? (
-        //     <ActivityIndicator
-        //       size="large"
-        //       color="#e95d2a"
-        //       style={{paddingBottom: 70}}
-        //     />
-        //   ) : (
-        //     noMoreProducts && (
-        //       <View
-        //         style={{
-        //           height: '50%',
-        //           justifyContent: 'center',
-        //           alignItems: 'center',
-        //         }}>
-        //         <Text style={{color: '#b1b1b1'}}>Product end</Text>
-        //       </View>
-        //     )
-        //   )
-        // }
-        renderItem={({item}) => <ProductItem product={item} />}
-        keyExtractor={(item, i) => i.toString()}
-        scrollEnabled
-      />
-    )}
-  </>
-  )
+              height: '100%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <ActivityIndicator
+              size="large"
+              color="#e95d2a"
+              style={{ backgroundColor: 'white' }}
+            />
+          </View>
+        </>
+      ) : (
+        <FlatList
+          nestedScrollEnabled={true}
+          alwaysBounceVertical
+          numColumns={2}
+          ItemSeparatorComponent={() => (
+            <View
+              style={{
+                width: '100%',
+                backgroundColor: '#f7f7f7',
+              }}
+            />
+          )}
+          data={filteredData} // Use filteredData as the data source
+          onEndReached={async () => {
+            await fetchMoreData();
+          }}
+          ref={refThreshold}
+          onEndReachedThreshold={10}
+          renderItem={({ item }) => <ProductItem product={item} />}
+          keyExtractor={(item, i) => i.toString()}
+          scrollEnabled
+        />
+      )}
+    </>
+  );
 }
