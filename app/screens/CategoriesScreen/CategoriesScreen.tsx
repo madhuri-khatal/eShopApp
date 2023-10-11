0ad7f4d29a6fcbfa397f3fb1d2121cfc30848f38
madhuri-khatal <madhuri.k@kitintellect.com>
2023-10-11 18:52:13 +0530

    create: category

--- app/screens/CategoriesScreen/CategoriesScreen.tsx
+++ app/screens/CategoriesScreen/CategoriesScreen.tsx
@@ -1,19 +1,71 @@
-import {HeaderBar} from '../../components/ui/HeaderBar';
+import React, {useState} from 'react';
+import {View, StyleSheet, ScrollView} from 'react-native';
+import {Button, List, useTheme} from 'react-native-paper';
+import {DrawerContentScrollView} from '@react-navigation/drawer';
 import {DrawerActions} from '@react-navigation/native';
-import {View} from 'react-native';
-import React, {useEffect, useState} from 'react';
-import {Appbar, Text} from 'react-native-paper';
-import {ScrollView} from 'react-native-gesture-handler';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {useProductContext} from '../../context/ProductContext';
 
 export const CategoriesScreen = (props: any) => {
+  //   const {navigation} = props;
+  // const{mainCategory}=useProductContext();
+  // console.log("mainCategory=======",mainCategory[0].name);
+  const [active, setActive] = useState<string>('');
+  const [expanded, setExpanded] = React.useState(true);
+  const {
+    mainCategory,
+    getSubCategoery,
+    subCategory,
+    setSubCategory,
+    getProductByCategoryId,
+  } = useProductContext();
   const {navigation} = props;
-
+  const handlePress = () => setExpanded(!expanded);
+  const theme = useTheme();
   return (
     <>
-      <ScrollView style={{backgroundColor: '#e95d2a'}}>
-        <View>
-          <Text style={{color: '#ffffff'}}>TITLE OF categories screen</Text>
-        </View>
+      <ScrollView
+        style={{backgroundColor: '#e95d2a'}}>
+        <List.Section style={{marginTop:50}}>
+          {mainCategory.map((categoery: any, index: number) => (
+            <List.Accordion
+              key={index}
+              // theme={theme}
+              titleStyle={{
+                color: '#ffffff',
+                fontSize: 21,
+                fontWeight: 'normal',
+                textAlign: 'center',
+                // backgroundColor: '#000000',
+              }}
+              style={{backgroundColor: '#e95d2a', width: '110%'}}
+              title={categoery?.name}
+              expanded={active == categoery?.id ? true : false}
+              onPress={() => {
+                setSubCategory([]);
+                getSubCategoery(categoery?.id);
+                setActive(categoery?.id);
+              }}>
+              {subCategory.map((data: any) => (
+                <List.Item
+                  titleStyle={{
+                    color: '#ffffff',
+                    fontSize: 18,
+                    textAlign: 'center',
+                  }}
+                  style={{marginLeft: 40, padding: 0}}
+                  title={data?.name}
+                  onPress={async () => {
+                    await getProductByCategoryId(data?.id);
+                    const id = data?.id;
+                    navigation.navigate('ProductsListScreen', {id});
+                    // navigation.getParent('LeftDrawer').dispatch(DrawerActions.closeDrawer());
+                  }}
+                />
+              ))}
+            </List.Accordion>
+          ))}
+        </List.Section>
       </ScrollView>
     </>
   );
