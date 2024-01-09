bfe468f57a7040e5a1d9312350792090399ee9d7
madhuri-khatal <117505759+madhuri-khatal@users.noreply.github.com>
2024-01-09 17:49:29 +0530

    Update HomeScreen.tsx

--- app/screens/HomeScreen/HomeScreen.tsx
+++ app/screens/HomeScreen/HomeScreen.tsx
@@ -1,9 +1,16 @@
 import {View, Image} from 'react-native';
-
+import {ScrollView} from 'react-native-gesture-handler';
 import React, {useEffect} from 'react';
 import {Appbar, FAB, Text} from 'react-native-paper';
 import {DrawerActions} from '@react-navigation/native';
-
+import FeaturedCategories from '../../components/Product/FeaturedCategories';
+import HomeWhyUs from '../../components/Product/HomeWhyUs';
+import CouponList from '../../components/Product/CouponList';
+import Festival from '../../components/Product/Festival';
+import Footer from '../../components/Product/Footer';
+import ListOFProducts from '../../screens/ProductScreen/ProductDetailsScreen/ListOFProducts';
+import {Caraousel} from '../../components/ui/Caraousel';
+import {CaraouselContent} from '../../components/ui/CaraouselContent';
 import {useProductContext} from './../../context/ProductContext';
 import {Linking} from 'react-native';
 
@@ -36,11 +43,46 @@ export default function HomeScreen({navigation}: any) {
           <Appbar.Content title="" />
           <Appbar.Action icon="menu" color="#506574" onPress={_handleMore} />
         </Appbar.Header>
-        <Text>home page
 
-        </Text>
-
-       
+        <ScrollView style={{marginBottom: 60}}>
+          <Caraousel
+            Component={CaraouselContent}
+            data={images}
+            autoPlay={true}
+            onSnapToItem={(i: number) => console.log()}
+          />
+          <FeaturedCategories />
+          <Festival />
+          <ListOFProducts />
+          <View style={{paddingVertical: 10}}>
+            <CouponList />
+          </View>
+          <Text
+            style={{
+              fontSize: 20,
+              fontWeight: 'bold',
+              lineHeight: 28,
+              padding: 10,
+              textAlign: 'center',
+              color: '#506574',
+            }}>
+            Why SHG E Shop
+          </Text>
+          <HomeWhyUs />
+          <Footer />
+        </ScrollView>
+        <FAB
+          icon="whatsapp"
+          color="#ffffff"
+          style={{
+            position: 'absolute',
+            margin: 10,
+            right: 10,
+            bottom: 75,
+            backgroundColor: '#e95d2a',
+          }}
+          onPress={whatsappSupport}
+        />
       </View>
     </>
   );
